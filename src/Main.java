d3b1061239324bcfee704d381cef0d42362a427b
Armando Boaca <55357189+Armmm17@users.noreply.github.com>
2023-10-16 08:43:05 +0200

    test clone

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         Persona p1 = new Persona("Gianni", 2000, "Maschio");
         System.out.println(p1.toString());
 
+        Persona p2 = (Persona) p1.clone();
+        p1.setSesso("Pansessuale");
+        System.out.println(p2.toString());
+
+
     }
 }
