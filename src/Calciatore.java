4155eabdd5c9302452822b037f9201831986ee4a
Armando Boaca <55357189+Armmm17@users.noreply.github.com>
2023-10-16 09:56:07 +0200

    feat: Added nScarpe Attributes and moethod to control, modified constructor clone etc

--- src/Calciatore.java
+++ src/Calciatore.java
@@ -3,8 +3,8 @@ public class Calciatore extends Persona{
     private int nMaglia;
     private int nGoal;
 
-    public Calciatore(String nome, int annoNascita, String sesso, String ruolo, int nMaglia, int nGoal) {
-        super(nome, annoNascita, sesso);
+    public Calciatore(String nome, int annoNascita, String sesso, int nScarpe, String ruolo, int nMaglia, int nGoal) {
+        super(nome, annoNascita, sesso, nScarpe);
         this.ruolo = ruolo;
         this.nMaglia = nMaglia;
         this.nGoal = nGoal;
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import java.util.concurrent.Callable;
 
 public class Main {
     public static void main(String[] args){
-        Persona p1 = new Persona("Gianni", 2000, "Maschio");
+        Persona p1 = new Persona("Gianni", 2000, "Maschio", 42);
         System.out.println(p1.toString());
 
         Persona p2 = (Persona) p1.clone();
         p2.setSesso("Pansessuale");
         System.out.println(p2.toString());
 
-        Calciatore c1 = new Calciatore("Giacomo", 2000, "Maschio", "Centrocampista", 17, 32);
+        Calciatore c1 = new Calciatore("Giacomo", 2000, "Maschio",44, "Centrocampista", 17, 32);
         System.out.println(c1.toString());
         c1.addGoal(5);
         Calciatore c2 = (Calciatore)c1.clone();
