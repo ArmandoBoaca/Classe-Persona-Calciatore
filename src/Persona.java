d61f10ab4a1dfc9c58cadf5a987c40c36f6753ba
Armando Boaca <55357189+Armmm17@users.noreply.github.com>
2023-10-16 09:36:33 +0200

    feat: Copy constructor persona

--- src/Persona.java
+++ src/Persona.java
@@ -9,6 +9,12 @@ public class Persona {
         this.sesso = sesso;
     }
 
+    public Persona(Persona persona){
+        this.nome = persona.nome;
+        this.annoNascita = persona.annoNascita;
+        this.sesso = persona.sesso;
+    }
+
     public String getNome() {
         return nome;
     }
