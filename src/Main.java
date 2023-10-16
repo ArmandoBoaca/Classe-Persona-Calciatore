9e5252c75c9bac6e67fb213c091d1a0d5e94b306
Armando Boaca <55357189+Armmm17@users.noreply.github.com>
2023-10-16 08:29:13 +0200

    Test added in main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         Persona p1 = new Persona("Gianni", 2000, "Maschio");
         System.out.println(p1.toString());
+        
     }
 }
